49a602706cdf6d31f2d5276c6371be48eb67cc83
Hans-Jörg Bibiko <bibiko@shh.mpg.de>
2020-07-07 13:26:20 +0200

    fixed tmpdir's LocalPath

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,4 +6,4 @@ from pathlib import Path
 @pytest.fixture
 def create_repo(tmpdir, autouse=True):
     with zipfile.ZipFile(str(Path(__file__).parent / 'repo_data.zip'), 'r') as z:
-        z.extractall(str(Path(tmpdir)))
+        z.extractall(Path(tmpdir))
