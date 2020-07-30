d5fbb9e53aa0897f1ac361eb677b829055b7a840
chrzyki <christoph@functor-argument.net>
2020-07-30 12:27:06 +0200

    bump version for development

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pynumerals',
-    version='1.0.0',
+    version='1.0.0.dev0',
     license='Apache 2.0',
     description='Helper library for numeralbank projects',
     long_description=open('README.md').read(),
--- src/pynumerals/__init__.py
+++ src/pynumerals/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.0.0"
+__version__ = "1.0.0.dev0"
 
 from pynumerals.errorcheck import *  # noqa: F401, F403
 from pynumerals.mappings import *  # noqa: F401, F403
